9fd34ef94ffea34fb986ca33e86f747226f795eb
Bonobo0 <47466832+Bonobo0@users.noreply.github.com>
2024-08-30 11:12:28 +0900

    Feat: Minigame2 event (#23)

---
+++ Assets/Scripts/Event/Minigame/BuildingFactoriesMinigame.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildingFactoriesMinigame : EventData
+{
+    public static BuildingFactoriesMinigame Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void OnEventTrigger()
+    {
+        base.OnEventTrigger();
+        SceneManager.LoadScene("MiniGame2Scene", LoadSceneMode.Additive);
+        // Building Factories Minigame
+    }
+    public override void OnEventEnd()
+    {
+        base.OnEventEnd();
+        // End of Building Factories Minigame
+        SceneManager.UnloadSceneAsync("MiniGame2Scene");
+    }
+}
